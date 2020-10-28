d12e4384ea4bf7444750ece3ed2a9c9c92044bc8
Richard Conway <richard@rconway.co.uk>
2020-10-28 11:22:39 +0000

    EOEPCA-186 Updates for deployment

---
+++ k8s/jupyter.tf
@@ -0,0 +1,81 @@
+resource "kubernetes_deployment" "jupyter" {
+  metadata {
+    name = "jupyter"
+
+    labels = {
+      app = "jupyter"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "jupyter"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "jupyter"
+        }
+      }
+
+      spec {
+        container {
+          name  = "jupyter"
+          image = "rconway/jupyter"
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "jupyter" {
+  metadata {
+    name = "jupyter"
+
+    labels = {
+      app = "jupyter"
+    }
+  }
+
+  spec {
+    port {
+      protocol    = "TCP"
+      port        = 80
+      target_port = "8888"
+    }
+
+    selector = {
+      app = "jupyter"
+    }
+
+    type = "NodePort"
+  }
+}
+
+resource "kubernetes_ingress" "jupyter" {
+  metadata {
+    name      = "jupyter"
+    namespace = "default"
+  }
+
+  spec {
+    rule {
+      host = "jupyter.test.192.168.49.2.nip.io"
+
+      http {
+        path {
+          backend {
+            service_name = "jupyter"
+            service_port = "80"
+          }
+        }
+      }
+    }
+  }
+}
+
